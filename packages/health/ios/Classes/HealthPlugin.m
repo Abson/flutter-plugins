353f024472879c44338984db5ead3c976b8e9a68
Abson <lizhi@Absons-macbookpro.local>
2025-06-26 18:11:16 +0800

    Can still use it legally when user do not use 'use_framework!' in Podfile of iOS

--- packages/health/ios/Classes/HealthPlugin.m
+++ packages/health/ios/Classes/HealthPlugin.m
@@ -1,5 +1,9 @@
 #import "HealthPlugin.h"
+#if __has_include(<health/health-Swift.h>)
 #import <health/health-Swift.h>
+#else
+#import "health-Swift.h"
+#endif
 
 @implementation HealthPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
